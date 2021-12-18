06190b931de25779d3cb3cf5750d61621834d23f
Ignacio SuarezB <suarezb.ignacio@gmail.com>
2021-12-18 11:17:01 -0300

    moving logic to itemlistcontainer

--- src/components/list/ItemList.jsx
+++ src/components/list/ItemList.jsx
@@ -1,24 +1,7 @@
-import { useEffect, useState } from "react";
 import Item from "./Item";
-import { useParams } from "react-router-dom";
 import Loader from "../general/Loader";
-import { getItems } from "../../services/services";
 
-const ItemList = () => {
-  const [items, setItems] = useState([]);
-  const { categoryId } = useParams();
-
-  useEffect(() => {
-    setItems([]);
-    const datosAPI = getItems();
-    datosAPI.then((itemsPromesa) => {
-      if (categoryId)
-        itemsPromesa = itemsPromesa.filter(
-          (item) => item.category === categoryId
-        );
-      setItems(itemsPromesa);
-    });
-  }, [categoryId]);
+const ItemList = ({ items }) => {
   return (
     <div className="row justify-content-center">
       {items.length === 0 ? (
--- src/components/list/ItemListContainer.jsx
+++ src/components/list/ItemListContainer.jsx
@@ -1,14 +1,30 @@
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
+import { getItems } from "../../services/services";
+import { useEffect, useState } from "react";
 
 const ItemListContainer = ({ greeting }) => {
+  const [items, setItems] = useState([]);
   const { categoryId } = useParams();
+
+  useEffect(() => {
+    setItems([]);
+    const datosAPI = getItems();
+    datosAPI.then((itemsPromesa) => {
+      if (categoryId)
+        itemsPromesa = itemsPromesa.filter(
+          (item) => item.category === categoryId
+        );
+      setItems(itemsPromesa);
+    });
+  }, [categoryId]);
+
   return (
     <section>
       <h1>
         {greeting} {categoryId}
       </h1>
-      <ItemList />
+      <ItemList items={items} />
     </section>
   );
 };
